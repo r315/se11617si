420bc6bfd896f128b0622916221b9e2b368b81c7
r315 <mreishugo@gmail.com>
2016-11-01 23:36:21 +0000

    Conversion of read and write functions to symbols

--- includes/gpio.h
+++ includes/gpio.h
@@ -10,28 +10,23 @@
 #define _GPIO_H_
 
 #include <stdint.h>
+#include <lpc2106.h>
+
+#define GPIO_Set(x) GPIO0->SET = x
+#define GPIO_Clr(x) GPIO0->CLR = x
+#define GPIO_Read() GPIO0->PIN
+#define GPIO_Write(x) GPIO0->PIN = x
+#define GPIO_OUTPUT	1
+#define GPIO_INPUT	0
+
 
 /**
  * @brief	Initialyze Port indicating output pins ans their state. 
  *			bits as 1 are configured as outputs
  */
 void GPIO_Init(uint32_t output, uint32_t state);
-
-/**
- * @brief	Read Port. 
- * @return	status of port 0 indicating the state of all 32bit.
- */
-uint32_t GPIO_Read(void);
-
-void GPIO_Write(uint32_t value);
-
-void GPIO_Set(uint32_t value);
-
-void GPIO_Clr(uint32_t value);
-
-void GPIO_Pin(uint8_t pin_number, uint8_t state);
-
-
+void GPIO_SetPin(uint8_t pin_number, uint8_t state);
+void GPIO_ConfigPin(uint32_t pin_number, uint32_t dir);
 
 #endif
 
--- sources/gpio/gpio.c
+++ sources/gpio/gpio.c
@@ -2,33 +2,20 @@
 #include <gpio.h>
 
 
-
 void GPIO_Init(uint32_t output, uint32_t state){
 	GPIO0->DIR = output;   /* Legacy version :( */	
 	GPIO0->PIN = state;
 }
 
-uint32_t GPIO_Read(void){
-	return GPIO0->PIN;
+void GPIO_ConfigPin(uint32_t pin_number, uint32_t dir){
+	GPIO0->DIR &= ~(1<<pin_number);
+	GPIO0->DIR |= (1<<pin_number);
 }
 
-void GPIO_Write(uint32_t value){
-	GPIO0->PIN = value;
-}
-
-void GPIO_Set(uint32_t value){
-	GPIO0->SET = value;
-}
-
-void GPIO_Clr(uint32_t value){
-	GPIO0->CLR = value;
-}
-
-void GPIO_Pin(uint8_t pin_number, uint8_t state){
+void GPIO_SetPin(uint8_t pin_number, uint8_t state){
 	if(state)
 		GPIO0->SET = (1<<pin_number);
 	else
 		GPIO0->CLR = (1<<pin_number);
 }
 
- 
