cd2bea7ba8c74eeddd4c2c429b7e9c7e72c4ca98
r315 <mreishugo@gmail.com>
2016-11-03 00:28:00 +0000

    test of io pins

--- sources/blink/blink.c
+++ sources/blink/blink.c
@@ -4,20 +4,22 @@
 #define ON 1
 #define OFF 0
 
-#define LED (1<<12)
+#define LED (1<<4)
 
 #define WAIT_200MS 200 //2,5Hz => T=400ms
 
 void delay(int time);
 
 int main(int argc, char *argv[]){
-int state = OFF;
-	GPIO_Init(LED,0);
+int state = 0;
+	//GPIO_Init(LED,0);
+	GPIO0->DIR = 0xFFFF;
 
 	while(1){
-		GPIO_Write(state == 0 ? LED : 0);
+		//GPIO_Write(state == 0 ? LED : 0);
+		GPIO0->PIN = state;
 		delay(WAIT_200MS);
-		state ^= 0x01;
+		state ^= 0xFFFF;
 	}
 	return 0;
 }
