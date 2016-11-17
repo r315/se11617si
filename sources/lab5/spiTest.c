96d9b2f6f1d2307c575de41bab5b12f16efd8c2e
Hugo Reis <hugo.reis@bithium.com>
2016-11-17 16:46:12 +0000

    spiTest initialize all libraries

--- sources/lab5/spiTest.c
+++ sources/lab5/spiTest.c
@@ -1,27 +1,31 @@
+
 #include <spi.h>
 #include <led.h>
+#include <lcd.h>
+#include <timer.h>
+#include <rtc.h>
+#include <button.h>
 
-#define DEV_CS (1<<3)
-
-void delayMs(uint32_t ms){
-volatile int dlms;
-
-	while(ms--){
-		dlms = 14745;
-		while(dlms--);
-	}
-}																			 
 
 int main(int argc, char *argv[]){
-
+struct tm rtc;
+	rtc.tm_sec = 0;
+	rtc.tm_min = 0;
+	rtc.tm_hour = 20;
+	rtc.tm_mday = 10;
+	rtc.tm_mon = 11;
+	rtc.tm_year = 2016;
+	rtc.tm_wday = 0;
+   
+   TIMER0_Init(MS_IN_1S);	
+  	LED_Init(LED, LED_OFF);
+  	BUTTON_Init();
+  	RTC_Init(&rtc);
 	SPI_Init(16, 8);
-	LED_Init(LED, LED_OFF);
+	LCD_Init();
 	
 	while(1){ 
-		LED_SetState(LED_OFF);
-		SPI_Send('U');		
-		LED_SetState(LED_ON);
-		delayMs(200);		
+		
 	}
 	return 0;
 }
