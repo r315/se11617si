e65ea15e95a4083aa9efd2770cbca7c2babb3aac
r315 <mreishugo@gmail.com>
2016-12-14 23:36:14 +0000

    implementation of basic sprite draw

--- sources/project/space.c
+++ sources/project/space.c
@@ -1,5 +1,7 @@
 #include <lcd.h>
 #include <stdint.h>
+#include <button.h>
+#include <timer.h>
 #include "space.h"
 
 
@@ -18,15 +20,73 @@ static const char title[]={
     "L|R -  move\n"
 };
 
+Sprite tank;
+int state;
 
-void popSpace(void){
+void drawSprite(Sprite *sp){
+uint16_t i;
+uint8_t *data;
+    if(sp->alive){
+        data = sp->data;                
+        LCD_Window(sp->x,sp->y,16,16);        
+        for(i = 0; i < SPRITE_SIZE / 2; i++, data+=1){
+            LCD_Data(pallete[*data & 15]);
+            LCD_Data(pallete[(*data)>>4 & 15]);
+        }
+        
+    }
+    
+}
+
+void eraseSprite(Sprite *sp){
+uint16_t i;
+uint8_t *data;    
+    data = sp->data;                
+    LCD_Window(sp->x,sp->y,16,16);        
+    for(i = 0; i < SPRITE_SIZE; i++, data+=1){
+        LCD_Data(BLACK);   //TODO: remove cs setting  on lcddata function
+    }   
+}
+
+void popSpace(void *ptr){
     LCD_Clear(BLACK);
     LCD_SetColors(RED,BLACK);
     LCD_Goto(0,0);    
     LCD_WriteString((char*)title);
-    LCD_SetColors(GREEN,BLACK);   
+    LCD_SetColors(GREEN,BLACK);  
+    
+    tank.data = (uint8_t*)spriteTank;
+    tank.x = (LCD_W/2)-8;
+    tank.y = LCD_H-16;
+    tank.alive = 1;
+    
+    
 }
 
 void space(int b){
+static uint32_t frametime;
+
+
+    if(frametime > TIMER0_GetValue())
+        return;    
+
+    switch(b){
+        case BUTTON_L:
+           eraseSprite(&tank);
+            tank.x = tank.x? tank.x-1 : tank.x;
+           drawSprite(&tank); 
+        break;
+        case BUTTON_R:
+            eraseSprite(&tank);
+            tank.x = tank.x < LCD_W - 16? tank.x+1 : tank.x;
+           drawSprite(&tank); 
+        break;
+        case BUTTON_F:    
+        
+        
+        default: break;
+    }
+    
+    frametime = TIMER0_GetValue() + 20;
 
 }
--- sources/project/space.h
+++ sources/project/space.h
@@ -1,7 +1,17 @@
 #ifndef _space_h_
 #define _space_h_
 
+#include <lcd.h>
+#define SPRITE_SIZE 16*16
 
-void popSpace(void);
+static const int pallete[] = { PINK,BLACK,RED,GREEN,BLUE,PINK,WHITE};
+
+static const char spriteTank [SPRITE_SIZE] = {0
+    
+};
+
+void popSpace(void *ptr);
 void space(int b);
+
+
 #endif
