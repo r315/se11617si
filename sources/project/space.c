7f3e06a538bd280ce22be4384447cf1ea41705ce
r315 <mreishugo@gmail.com>
2016-12-18 02:19:18 +0000

    Tank move an fire

--- sources/project/space.c
+++ sources/project/space.c
@@ -3,49 +3,117 @@
 #include <button.h>
 #include <timer.h>
 #include "space.h"
+#include "sprites/sprites.h"
 
+#include <gpio.h>
+#include <spi.h>
 
-typedef struct _sprite{
+void LCD_Fill(uint16_t color, uint32_t n);
+
+typedef struct _tank{
     int x;
     int y;
-    int alive;
-    int color;
     uint8_t *data;
-}Sprite;
+}Tank;
+
+typedef struct _alien{
+	int x;
+	int y;
+	uint8_t *data;
+}Alien;
+
+typedef struct projectile{
+	int x;
+	int y;
+	uint8_t inmotion;
+}Projectile;
 
 static const char title[]={
-    "           GAME\n\n"
-    "F   -  fire\n"
-    "S   -  save & exit\n"
-    "L|R -  move\n"
+    "      # Space Invaders #\n\n"    
 };
 
-Sprite tank;
+void LCD_Data(uint16_t color);
+
+Tank tank;
+Projectile projectiles[5];
+
+Alien aliens[10];
+
 int state;
 
-void drawSprite(Sprite *sp){
+void drawTank(Tank *tk){
 uint16_t i;
 uint8_t *data;
-    if(sp->alive){
-        data = sp->data;                
-        LCD_Window(sp->x,sp->y,16,16);        
-        for(i = 0; i < SPRITE_SIZE / 2; i++, data+=1){
-            LCD_Data(pallete[*data & 15]);
-            LCD_Data(pallete[(*data)>>4 & 15]);
-        }
-        
-    }
-    
+	
+	data = tk->data;                
+	LCD_Window(tk->x,tk->y,16,16);        
+	//GPIO_Clr(3);
+	//GPIO_Set(9);
+	for(i = 0; i < SPRITE_SIZE; i++, data+=1){
+		//LCD_Data(pallete[*data & 15]);
+		//LCD_Data(pallete[(*data)>>4 & 15]);
+		//SPI_Send(pal1[*data & 15]);
+		//SPI_Send(pal1[(*data)>>4 & 15]);
+		LCD_Data(pal1[*data]);
+	}
+	//GPIO_Set(3);
 }
 
-void eraseSprite(Sprite *sp){
-uint16_t i;
-uint8_t *data;    
-    data = sp->data;                
-    LCD_Window(sp->x,sp->y,16,16);        
-    for(i = 0; i < SPRITE_SIZE; i++, data+=1){
-        LCD_Data(BLACK);   //TODO: remove cs setting  on lcddata function
-    }   
+void moveTank(Tank *tk, int8_t dir){
+int newx;
+int8_t d;
+
+	newx = tk->x + dir;
+	
+	if(newx > (SCREEN_W-SPRITE_W) || newx < 0)
+		return;
+		
+	d = (dir < 0)? -dir : dir;
+
+	// erase only necessary data from old position		
+	if(newx >= tk->x)
+		LCD_Window(tk->x, tk->y, d, SPRITE_H);
+	else
+		LCD_Window(newx+SPRITE_W, tk->y, d, SPRITE_H);	
+		
+	LCD_Fill(BGCOLOR, d * SPRITE_H);
+	
+	tk->x = newx;
+	drawTank(tk);	
+}
+
+void moveProjectile(Projectile *pj, int8_t dir){
+int newy;
+
+	if(!pj->inmotion)
+		return;
+		
+	newy = pj->y + dir;
+	
+	if(newy > (SCREEN_H-SPRITE_H) || newy < 0){
+		pj->inmotion = 0;
+		return;
+	}
+	
+	LCD_Window(pj->x, pj->y, 1, 4);
+	LCD_Fill(BGCOLOR, 4);	
+	
+	pj->y = newy;
+	
+	LCD_Window(pj->x, pj->y, 1, 4);
+	LCD_Fill(WHITE, 4);		
+}
+
+void newProjectile(Projectile *pjs, uint8_t n, int x, int y){
+uint8_t i;
+	for(i = 0; i < n; i++){
+		if(!pjs[i].inmotion){
+			pjs[i].x = x;
+			pjs[i].y = y;
+			pjs[i].inmotion = ON;
+			return;
+		}	
+	}	
 }
 
 void popSpace(void *ptr){
@@ -55,38 +123,37 @@ void popSpace(void *ptr){
     LCD_WriteString((char*)title);
     LCD_SetColors(GREEN,BLACK);  
     
-    tank.data = (uint8_t*)spriteTank;
+    tank.data = (uint8_t*)spcinv;
     tank.x = (LCD_W/2)-8;
     tank.y = LCD_H-16;
-    tank.alive = 1;
-    
-    
+    drawTank(&tank);    
 }
 
 void space(int b){
-static uint32_t frametime;
-
-
-    if(frametime > TIMER0_GetValue())
-        return;    
-
+static uint32_t frametime;	
+uint8_t n;
+	
+    if(TIMER0_GetValue() > frametime){
+		for(n=0; n < MAX_PROJECTILES; n++)		
+			moveProjectile(&projectiles[n],-4);
+			
+		frametime = TIMER0_GetValue() + 20;
+	}
+	else{
+		if(BUTTON_GetEvents() == BUTTON_HOLD)
+		return;    
+	}
+	
     switch(b){
-        case BUTTON_L:
-           eraseSprite(&tank);
-            tank.x = tank.x? tank.x-1 : tank.x;
-           drawSprite(&tank); 
+        case BUTTON_L:	moveTank(&tank,-2); break;
+        case BUTTON_R:	moveTank(&tank,2); break;            
         break;
-        case BUTTON_R:
-            eraseSprite(&tank);
-            tank.x = tank.x < LCD_W - 16? tank.x+1 : tank.x;
-           drawSprite(&tank); 
-        break;
-        case BUTTON_F:    
-        
+        case BUTTON_F: newProjectile(projectiles, MAX_PROJECTILES, tank.x + SPRITE_W/2, tank.y - 4);
+				
         
         default: break;
     }
     
-    frametime = TIMER0_GetValue() + 20;
+    
 
 }
--- sources/project/space.h
+++ sources/project/space.h
@@ -1,14 +1,20 @@
 #ifndef _space_h_
 #define _space_h_
 
-#include <lcd.h>
-#define SPRITE_SIZE 16*16
+#define SCREEN_W LCD_W
+#define SCREEN_H LCD_H
 
-static const int pallete[] = { PINK,BLACK,RED,GREEN,BLUE,PINK,WHITE};
+#define SPRITE_H 8
+#define SPRITE_W 16
+#define SPRITE_SIZE (SPRITE_W * SPRITE_H)
 
-static const char spriteTank [SPRITE_SIZE] = {0
-    
-};
+#define BGCOLOR BLACK
+
+#define ON 1
+#define OFF 0
+
+#define MAX_PROJECTILES 5
+
 
 void popSpace(void *ptr);
 void space(int b);
