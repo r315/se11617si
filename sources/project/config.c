5cabb14b3c4af8c633058d116768fc1566f2a717
r315 <mreishugo@gmail.com>
2016-12-18 02:15:08 +0000

    Test version of config, todo fix field indicator and save to flash

--- sources/project/config.c
+++ sources/project/config.c
@@ -1,15 +1,26 @@
 #include <time.h>
 #include <lcd.h>
 #include <rtc.h>
+#include <button.h>
 #include "config.h"
 
+//TODO: fix field indicator and save to flash
+
+
+static struct tm alarm, cur_time;
+static uint8_t field;
 
 static const char title[]={
     "           CONFIG\n\n"
     
 };
 
-void popConfig(void *ptr){   
+void popConfig(void *ptr){  
+	field = HOUR;
+	
+	RTC_GetValue(&cur_time);
+	RTC_GetAlarmValue(&alarm);
+ 
     LCD_Clear(BLACK);
     LCD_SetColors(RED,BLACK);
     LCD_Goto(0,0);    
@@ -18,8 +29,105 @@ void popConfig(void *ptr){
     
 }
 
+void PRINT_RtcTime(struct tm *rtc, uint8_t format){
+    LCD_WriteInt(rtc->tm_hour, TIME_FORMAT);
+    LCD_WriteChar(':');
+    LCD_WriteInt(rtc->tm_min, TIME_FORMAT);
+    if(format == RTC_TIME_HHMMSS){
+        LCD_WriteChar(':');
+        LCD_WriteInt(rtc->tm_sec, TIME_FORMAT);
+    }
+}
+
+const char wdays[7][4]={"mon","tue","wed","thu","fri","sat","sun"};
+void PRINT_Rtc(struct tm *rtc, uint8_t data){   
+   switch(data){      
+      case RTC_DATETIME:
+         PRINT_RtcTime(rtc,RTC_TIME_HHMM);
+         LCD_WriteChar(' ');
+                  
+      case RTC_DATE:
+         LCD_WriteInt(rtc->tm_mday, TIME_FORMAT);
+         LCD_WriteChar('-');
+         LCD_WriteInt(rtc->tm_mon, TIME_FORMAT);
+         LCD_WriteChar('-');
+         LCD_WriteInt(rtc->tm_year, (4<<8) |10);
+         break;     
+         
+      case RTC_TIME_HHMM:
+         PRINT_RtcTime(rtc, RTC_TIME_HHMM);
+         break;
+      
+      case RTC_TIME_HHMMSS:
+         PRINT_RtcTime(rtc,RTC_TIME_HHMMSS);        
+         break;      
+         
+      case RTC_WDAY:         
+         LCD_WriteString((char*)&wdays[rtc->tm_wday][0]);         
+         break;      
+   }   
+}
+
+//TODO: FIX this
+void changeField(int32_t *fld, uint32_t max, int8_t step){
+	*fld = (*fld + step);
+	*fld = *fld % (max+1);
+	
+	if(*((uint32_t*)fld) > max)
+		*fld = max;	
+}
+
+void setTime(struct tm *rtc, uint8_t fld, signed char step){
+  
+	switch(fld){
+		case HOUR: 		changeField((int32_t *)&rtc->tm_hour,23, step);	break;
+         
+		case MINUTES:	changeField((int32_t *)&rtc->tm_min,59, step);	break;		
+		 
+		case WEEKDAY:	changeField((int32_t *)&rtc->tm_wday,6, step);	break;
+			
+		case MDAY:		changeField((int32_t *)&rtc->tm_mday,31, step);	break;
+			
+		case MONTH:		changeField((int32_t *)&rtc->tm_mon,12, step);	break;
+			
+		case YEAR:		changeField((int32_t *)&rtc->tm_year,2060, step);	break;      
+   }
+   
+}
+
+void setIndicator(uint8_t fld){
+	LCD_Goto(8+(16*fld),80);
+    LCD_WriteChar('^');    
+}
+
 int config(int b){
+signed char step;
 
-
-return 1;
+	switch(b){
+		case BUTTON_F: field++;	break;
+			
+		case BUTTON_EMPTY: return 1;
+			
+		case BUTTON_S: return 0;
+			
+		case BUTTON_L: step = -1; break;
+		
+		case BUTTON_R: step =  1; break;
+	}
+	
+	if(field == YEAR +1)
+		return 0;
+		
+	setTime(&cur_time, field, step);    
+   
+	LCD_Goto(16,64);	   
+	PRINT_Rtc(&cur_time, RTC_TIME_HHMM);                
+	LCD_WriteChar(' ');
+	PRINT_Rtc(&cur_time, RTC_WDAY);      
+	LCD_WriteChar(' ');
+	PRINT_Rtc(&cur_time, RTC_DATE);      
+	LCD_WriteString("                 ");
+	setIndicator(field);	   
+	
+	return 1;
 }
--- sources/project/config.h
+++ sources/project/config.h
@@ -1,6 +1,25 @@
 #ifndef _config_h_
 #define _config_h_
 
+enum TimeFields{
+   HOUR,
+   MINUTES,   
+   WEEKDAY,   
+   MDAY,
+   MONTH,
+   YEAR   
+};
+
+enum RtcFormat{
+   RTC_DATETIME,
+   RTC_TIME_HHMM,
+   RTC_TIME_HHMMSS,
+   RTC_DATE, 
+   RTC_WDAY  
+};
+
+#define ENTER_CONFIG_TIME 2000 //2 seconds
+
 void popConfig(void *ptr);
 int config(int b);
 #endif
