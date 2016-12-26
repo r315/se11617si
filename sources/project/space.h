ded884878d88e9f39b2c5a510a286401e9747d3c
r315 <mreishugo@gmail.com>
2016-12-26 18:36:51 +0000

    CHG: Limit game screen to 128x128

--- sources/project/space.h
+++ sources/project/space.h
@@ -1,8 +1,11 @@
 #ifndef _space_h_
 #define _space_h_
 
-#define SCREEN_W LCD_W
-#define SCREEN_H LCD_H
+#define SCREEN_W 128
+#define SCREEN_H 128
+
+#define SCREEN_SX (LCD_W/2) - (SCREEN_W/2)
+#define SCREEN_SY (LCD_H/2) - (SCREEN_H/2)
 
 #define SPRITE_H 8
 #define SPRITE_W 16
@@ -14,6 +17,9 @@
 #define OFF 0
 
 #define MAX_PROJECTILES 5
+#define MAX_ALIENS 6*6
+#define SPRITES_DATA spcinv
+#define TANK_DATA SPRITES_DATA + (SPRITE_SIZE * 6)
 
 
 void popSpace(void *ptr);
