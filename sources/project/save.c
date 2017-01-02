41fb740082df29546097816724cb5c704d91a004
r315 <mreishugo@gmail.com>
2017-01-02 00:20:14 +0000

    implementation of data save to flash

--- sources/project/save.c
+++ sources/project/save.c
@@ -1,10 +1,21 @@
+#include <stdint.h>
+#include <flash.h>
 #include "save.h"
 
-
-int saveGame(void *save, int saveSize){
-    return 0;
+int saveData(void *data, int dataSize){
+    #ifndef _EMU_
+    return FLASH_WriteData(SAVE_BLOCK,data,dataSize);
+    #else
+    uint8_t *dst = SAVE_BLOCK;
+    while(dataSize--)
+        *dst++ = *((uint8_t*)data++);                
+    return CMD_SUCESS;
+    #endif
 }
 
-int loadGame(void *save, int saveSize){
-    return 0;
+int restoreData(void *data, int dataSize){
+uint8_t *src = SAVE_BLOCK;
+    while(dataSize--)
+        *((uint8_t*)data++) = *src++;        
+    return CMD_SUCESS;
 }
--- sources/project/save.h
+++ sources/project/save.h
@@ -1,7 +1,14 @@
 #ifndef _save_h_
 #define _save_h_
 
-int saveGame(void *save, int saveSize);
-int loadGame(void *save, int saveSize);
+int saveData(void *data, int dataSize);
+int restoreData(void *data, int dataSize);
+
+#ifndef _EMU_
+#define SAVE_BLOCK (uint32_t)((FLASH_LAST_SECTOR - 2) * FLASH_SECTOR_SIZE)
+#else
+static uint8_t SAVE_BLOCK[1024];
+#endif
+
 
 #endif
