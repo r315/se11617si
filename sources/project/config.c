8fa740310ef9e67e391b1bbc27a2c032cf4992d5
r315 <mreishugo@gmail.com>
2017-01-04 22:27:16 +0000

    No changes

--- sources/project/config.c
+++ sources/project/config.c
@@ -91,14 +91,17 @@ signed char step;
             if((++field) == (YEAR + 1))
                 field = HOUR;
             step = 0;
-            break;        
-        case BUTTON_S: return 0;        
+            break;
+
+        case BUTTON_S:
+            return 0;
+
         default: return 1;
-    } 
-   
+    }
+
     setTime(cur_time, field, step);
-    
+
     PRINT_FullDate(cur_time, field);    
-    
+
     return 1;
 }
