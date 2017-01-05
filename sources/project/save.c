edc31b76039050432974155cdae92bb53942f7ec
r315 <mreishugo@gmail.com>
2017-01-05 00:09:44 +0000

    ADD: Verification of written data

--- sources/project/save.c
+++ sources/project/save.c
@@ -1,12 +1,15 @@
+#include <string.h>
 #include <stdint.h>
 #include <flash.h>
 #include "save.h"
 
+
 int saveData(void *data, int dataSize){    
-    return FLASH_WriteData(SAVE_BLOCK,data,dataSize);    
+    FLASH_WriteData(SAVE_BLOCK, data, dataSize);
+    return FLASH_VerifyData(SAVE_BLOCK, data, dataSize); 
 }
 
 int restoreData(void *data, int dataSize){
-    memcpy(data,SAVE_BLOCK, dataSize);
+    memcpy(data, SAVE_BLOCK, dataSize);
     return CMD_SUCESS;
 }
